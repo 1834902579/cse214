e7093d6fccd2f68dc1d9583c95b97119f664706c
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-08-06 01:55:17 -0700

    Add files via upload

---
+++ lab_class_test_1+coding_test/1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void input_element_array(int *a,int len);
+
+
+int main(){
+
+    int *p;
+    int arr[100];
+    int size = 0;
+
+    p = &arr[0];
+
+   input_element_array(p,size);
+
+
+    return 0;
+}
+
+void input_element_array(int *a,int len){
+
+printf("Please Enter the Element of this array: \n");
+   int i=0;
+ for(i=0; scanf("%d",(a+i))!=EOF; i++){
+        len++;
+        }
+
+ for(i=0; i<len; i++){
+     printf("%d ",*(a+i));
+        }
+
+}
+
