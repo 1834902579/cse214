4e7a2435b4a37f5b2f5d9ee2ecd8b32c06b555bf
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-08-06 01:57:53 -0700

    Update 1.c

--- lab_class_test_1+coding_test/1.c
+++ lab_class_test_1+coding_test/1.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
-void input_element_array(int *a,int len);
+void inputElementOfArrayAndDisplay(int *a,int len);
 
 
 int main(){
 
     int *p;
-    int i;
     int arr[100];
     int size = 0;
 
     p = &arr[0];
 
-   input_element_array(p,size);
+   inputElementOfArrayAndDisplay(p,size);
 
 
     return 0;
 }
 
-void input_element_array(int *a,int len){
+void inputElementOfArrayAndDisplay(int *a,int len){
 
 printf("Please Enter the Element of this array: \n");
    int i=0;
- for(i=0; scanf("%d",*(a+i))!=EOF; i++){
+ for(i=0; scanf("%d",(a+i))!=EOF; i++){
         len++;
         }
 
  for(i=0; i<len; i++){
-     printf("%d ",(a+i));
+     printf("%d ",*(a+i));
         }
 
 }
